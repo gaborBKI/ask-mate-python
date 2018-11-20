d8617c36c47812e3c39bcc9bce064088493becdf
Gabor Buki <gabor.bki@gmail.com>
2018-11-20 12:51:48 +0100

    Added Font Awesome icons to question, formatted system time to readable time in list and in question as well.

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 
+import time
 import data_manager
 import connection
 import util
@@ -10,11 +11,21 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def route_list():
-    return render_template('list.html')
+    questions = data_manager.get_all_data('question.csv')
+    for question in questions:
+        question[1] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(question[1])))
+    return render_template('list.html', questions = questions)
 
+@app.route('/question/<int:qid>')
+def route_question(qid):
+    questions = data_manager.get_all_data('question.csv')
+    for question in questions:
+        if qid == int(question[0]):
+            question[1] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(question[1])))
+            return render_template('question.html', question = question)
 
 if __name__ == '__main__':
     app.run(
-        port=5000,
+        port=8000,
         debug=True,
     )
